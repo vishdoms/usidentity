5019e3d7694779e88232158b239a2c1472294446
vishdoms <vishdikki@gmail.com>
2025-07-27 15:11:58 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,7 @@
 
-author = 'Your Name'
-release = ''
 
 
-extensions = []
 
-exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
@@ -13,6 +9,31 @@ html_css_files = ['custom.css']
 html_sidebars = {'**': []}
 html_title = ""
 html_js_files = ['hide-version.js']
+
+
+# Configuration file for the Sphinx documentation builder
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = ''
+copyright = '2025'
+author = 'mike'
+
+release = '1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
 
 
 
